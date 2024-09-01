de31811b7088e6114876ecfd17c4ebd44e7c6a50
jacobwaurand <jacobwaurand@gmail.com>
2024-09-01 11:32:14 -0400

    fix error

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -6,7 +6,7 @@
   <div class="bg-surface-700 flex flex-col w-20 content-center justify-start">
     <div v-for="index in 5" class="w-full h-20 p-3 flex flex-col align-middle text-center font-bold hover:bg-secondary hover:text-surface-900">
       <font-awesome-icon class="grow m-1" icon="coffee"/>
-      Icon
+      Icon {{ index }}
     </div>
   </div>
 </template>
