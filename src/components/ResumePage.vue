19c066ecd170629993022e2c2f8a69f1f4a1c624
jacobwaurand <jacobwaurand@gmail.com>
2024-09-18 22:33:00 -0400

    Resume Page Update

--- src/components/ResumePage.vue
+++ src/components/ResumePage.vue
@@ -12,13 +12,46 @@ export default defineComponent({
 </script>
 
 <template>
+  <!-- Contact Info -->
   <div class="bg-surface-900 p-4 rounded-md">
-    <div class="text-4xl font-bold mb-2">Jacob Aurand</div>
+    <div class="text-2xl font-bold mb-2">Jacob Aurand</div>
     <div class="text-sm font-bold">
-      <span class="text-surface-400 mr-2 hover:text-surface-100"><font-awesome-icon class="mr-2" icon="envelope"/>Jacobwaurand@gmail.com</span>
-      <span class="text-surface-400 mr-2 hover:text-surface-100"><font-awesome-icon class="mr-2" icon="phone"/>770-630-3091</span>
-      <span class="text-surface-400 mr-2 hover:text-surface-100"><font-awesome-icon class="mr-2" icon="globe"/>Atlanta, Georgia</span>
+      <span class="text-surface-400 mr-2 hover:text-surface-100 cursor-pointer"><font-awesome-icon class="mr-2" icon="envelope"/>Jacobwaurand@gmail.com</span>
+      <span class="text-surface-400 mr-2 hover:text-surface-100 cursor-pointer"><font-awesome-icon class="mr-2" icon="phone"/>770-630-3091</span>
+      <span class="text-surface-400 mr-2 hover:text-surface-100 cursor-pointer"><font-awesome-icon class="mr-2" icon="globe"/>Atlanta, Georgia</span>
     </div>
+  </div>
+  <!-- Work Experience -->
+  <div class="bg-surface-900 p-4 rounded-md mt-3">
+    <div class="text-2xl font-bold">Work Experience</div>
+    <div class="font-bold text-surface-300 mt-3">Nasdaq - Evestment<span class="text-surface-400 ml-2 italic">(2022 - Current)</span></div>
+    <ul class="list-disc ml-4 mt-2">
+      <li>Designed and deployed new features in a microservices-driven ecosystem, creating new features for clients.</li>
+      <li>Mentored 2 junior developers this year to educate them on best practices and share my knowledge of our platform.</li>
+      <li>Identified 10+ performance bottlenecks and designed and built solutions to improve our platform speed by roughly 20%.</li>
+    </ul>
+    <div class="font-bold text-surface-300 mt-3">PolicyReporter<span class="text-surface-400 ml-2 italic">(2021 - 2022)</span></div>
+    <ul class="list-disc ml-4 mt-2">
+      <li>Designed and deployed new features in a microservices-driven ecosystem, creating new features for clients.</li>
+      <li>Mentored 2 junior developers this year to educate them on best practices and share my knowledge of our platform.</li>
+      <li>Identified 10+ performance bottlenecks and designed and built solutions to improve our platform speed by roughly 20%.</li>
+    </ul>
+    <div class="font-bold text-surface-300 mt-3">Canary Insights<span class="text-surface-400 ml-2 italic">(2020 - 2021)</span></div>
+    <ul class="list-disc ml-4 mt-2">
+      <li>Designed and deployed new features in a microservices-driven ecosystem, creating new features for clients.</li>
+      <li>Mentored 2 junior developers this year to educate them on best practices and share my knowledge of our platform.</li>
+      <li>Identified 10+ performance bottlenecks and designed and built solutions to improve our platform speed by roughly 20%.</li>
+    </ul>
+  </div>
+  <!-- Education -->
+  <div class="bg-surface-900 p-4 rounded-md mt-3">
+    <div class="text-2xl font-bold mb-2">Education</div>
+    <div class="font-bold text-surface-300">Georgia College & State University</div>
+    <ul class="list-disc ml-4 mt-2">
+      <li>Designed and deployed new features in a microservices-driven ecosystem, creating new features for clients.</li>
+      <li>Mentored 2 junior developers this year to educate them on best practices and share my knowledge of our platform.</li>
+      <li>Identified 10+ performance bottlenecks and designed and built solutions to improve our platform speed by roughly 20%.</li>
+    </ul>
   </div>
 </template>
 
